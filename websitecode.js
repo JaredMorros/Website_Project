37c8e3e43c625bbacf176aff70f3d69386296f52
GenUKJared <JaredM@genstudents.org>
2025-03-19 14:33:12 +0000

    Edited Audio

--- websitecode.js
+++ websitecode.js
@@ -2,14 +2,18 @@ var surprising = false
 
 function Surprise(){
     const button = document.querySelector('button')
+    const surpriseaudio = document.getElementById("surpriseaudio")
     if (surprising == false){
         document.getElementById("surprise").style.display = "block";
         surprising = true;
         button.innerText = "Unsurprise me!";
+        surpriseaudio.load();
+        surpriseaudio.play();
     }
     else{
         document.getElementById("surprise").style.display = "none";
         surprising = false;
         button.innerText = "Surprise me!";
+        surpriseaudio.pause();
     }
 }
