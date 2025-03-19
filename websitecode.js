e79e160b283e35e9f0e3e6a829bee9a6f141e380
GenUKJared <JaredM@genstudents.org>
2025-03-19 12:11:20 +0000

    Button can now read unsurprise me!

--- websitecode.js
+++ websitecode.js
@@ -1,12 +1,15 @@
 var surprising = false
 
 function Surprise(){
+    const button = document.querySelector('button')
     if (surprising == false){
         document.getElementById("surprise").style.display = "block";
-        surprising = true
+        surprising = true;
+        button.innerText = "Unsurprise me!";
     }
     else{
         document.getElementById("surprise").style.display = "none";
-        surprising = false
+        surprising = false;
+        button.innerText = "Surprise me!";
     }
 }
